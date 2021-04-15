ad4cc6532c52d912e474305a38e60e89b136bca6
Ruowen Tang <ruowen.tang@gmail.com>
2021-04-15 14:02:17 -0400

    Correct student and interview hardcoded display to api data

--- stories/index.js
+++ stories/index.js
@@ -148,6 +148,7 @@ storiesOf("Appointment", module)
   ))
   .add("Show", () => (
     <Show
+      interviewer={interviewer.name}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />
