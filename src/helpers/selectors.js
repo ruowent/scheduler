dcd7cea73a490324ae3f116f19e29156a6e4bd0c
Ruowen Tang <ruowen.tang@gmail.com>
2021-04-15 21:12:47 -0400

    Add getInterviewersForDay helper function and pass all tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,43 +1,30 @@
 export function getAppointmentsForDay(state, day) {
-  // let result = [];
-  // if (!state.days.length) {
-  //   return result;
-  // }
-  // for (let dayObj of state.days) {
-  //   if (dayObj.name === day) {
-  //     for (let appointment of dayObj.appointments) {
-  //       for (let key in state.appointments) {
-  //         if (appointment == key) {
-  //           result.push(state.appointments[key]);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  // return result;
-
   const dayFound = state.days.find(dayObj => dayObj.name === day)
-
   if (!dayFound) {
     return [];
   }
-
   const appointments = dayFound.appointments.map(appointmentId => state.appointments[appointmentId]);
-
   return appointments;
 };
-
-
 
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
   }
-
   for (let id in state.interviewers) {
     if (interview.interviewer == id) {
       interview.interviewer = state.interviewers[id];
     }
   }
   return interview;
+}
+
+export function getInterviewersForDay(state, day) {
+  const dayFound = state.days.find(dayObj => dayObj.name === day)
+  if (!dayFound) {
+    return [];
+  }
+
+  const interviewers = dayFound.interviewers.map(interviewerId => state.interviewers[interviewerId]);
+  return interviewers;
 }
