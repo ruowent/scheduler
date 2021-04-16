96ec7a591b0697d9bbc7edb5ecaf81d4df2cfe51
Ruowen Tang <ruowen.tang@gmail.com>
2021-04-15 20:24:31 -0400

    Add EMPTY, SHOW, CREATE useState function to the file

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,20 +3,28 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
+  console.log(props)
   return (
     <article className="appointment">
       <Header time={props.time} />
       {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />} */}
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+        />
+      )}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
